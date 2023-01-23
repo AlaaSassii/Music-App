ad547f0eca109052e9e9c2cfd2f8f7816f3bf6a4
alaasassii <alaasassijs@gmail.com>
2023-01-23 22:01:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useEffect, useRef} from 'react'
+import audio from './a.mp3'
 function App() {
+  const audioRef = useRef({})
+  console.log(audioRef.current.autoPlay);
+  const currentTime = ( time)=> {
+    const time1 = Number(time.toString().toFixed(0)) ; 
+    const min = time / 60 ; 
+    const sec = time % 60 ; 
+    return `${min}:${sec}`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <audio ref={audioRef}  controls={true} play={true} src={audio}></audio>
+      <button onClick={()=>{audioRef.current.muted = true}}>muted</button>
+      <button onClick={()=>{audioRef.current.loop = true}}>loop</button>
+      <button onClick={()=>{audioRef.current.controls = false}}>controls</button>
+      <button onClick={()=>{audioRef.current.play()}}>play</button>
+      <button onClick={()=>{audioRef.current.pause()}}>pause</button>
+      <button onClick={()=>{console.log(audioRef.current.duration)}}>duration</button>
+      <button onClick={()=>{console.log(currentTime(audioRef.current.currentTime) )}}>currentTime </button>
+      
+      </>
   );
 }
 
